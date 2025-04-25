cfa3ee190fcc9af3354a3eadd9c09f9572150b09
Riceko <nathanbchenwang@gmail.com>
2025-04-25 11:40:46 -0700

    changed the home page for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 // import Carousel from "@/components/home/GalleryPreview";
+const Home = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      Hello World
+    </div>
+  );
+};
 
+export default Home;
 // const Home = () => {
 //   return (
 //     <div className="flex h-screen w-screen items-center justify-center">
