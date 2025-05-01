21ec3037bc1a77c2129754f4fb998eca5a48bd57
cmakkkk <146979512+cmakkkk@users.noreply.github.com>
2025-04-30 18:55:51 -0700

    Started the constructor for the Review class

---
+++ data/Review.py
@@ -0,0 +1,9 @@
+class Review:
+    sentiment_score = 0
+
+    def __init__(self, rating, text, date_posted):
+        self.rating = rating
+        self.text = text
+        self.date_posted = date_posted
+
+        # the constructor should be able to analyise the sentiment as soon as it gets the text
