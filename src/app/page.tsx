2fd298e3d05fd6919e7a5b8e13e5bb7cf422da4b
Kimberly Monteagudo <kimberlymonteagudo@ucr-secure-25-10-13-149-62.wnet.ucr.edu>
2025-05-08 16:25:20 -0700

    Add link to the Google Form on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Display from "@/components/display";
 import TopReviewed from "@/components/TopReviewed";
-
 export default function HomePage() {
   return (
     <main className="main">
@@ -33,10 +32,22 @@ export default function HomePage() {
         </ol>
       </section>
 
+      {/* Add the button linking to the Google Form */}
+      <section className="section">
+        <h2 className="section-title">Submit Your Review</h2>
+        <a href="https://docs.google.com/forms/d/1ieV9hByRgtBzp4mrEwRcNBNFnLC9f4XfZ6RPoa-NrD4/viewform?edit_requested=true" target="_blank" rel="noopener noreferrer">
+          <button className="primary-button">
+            Google form
+          </button>
+        </a>
+      </section>
+
       <section className="section">
         <h2 className="section-title">All Courses</h2>
         <Display />
       </section>
+
+      
 
       <footer className="footer">
         <p>&copy; TBD</p>
