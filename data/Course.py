fbef5a51824d584a2e28882d8407619be1487606
cmakkkk <146979512+cmakkkk@users.noreply.github.com>
2025-05-15 21:01:47 -0700

    Finish up Review class tests

--- data/Course.py
+++ data/Course.py
@@ -55,18 +55,3 @@ class Course:
     
     def get_comment_summary(self, review_list):
         return self.comment_summary
-      
-
-# temporary tests
-#TODO: make more official and clean tests
-# review_1 = Review(4, "Very general chemistry, just know your stuff", "3/29/2017 14:10:31")
-# review_2 = Review(4, "If you passed the AP Chemistry Test in high school, this class will be a good review for you since it will cover topics from the different models of atoms, types of bonding, intermolecular forces, and stoichiometry. (You should still cover the material so you have a good foundation and an easy time in the class though) If possible, I would recommend taking it with Fokwa. Has a thick accent but sets up the class in a way that makes it super difficult to fail (you seriously need to not do anything to not get points). He has a redemption system so you can get points back if your final exam grade is much higher than your midterm exam scores. All of his exams have extra credit problems that he will give you to do before the exam day (yay!), but he has no curve. So scores on the exams depend entirely up to you. Homework is an easy 100% as long as you do it before the deadline and gives you unlimited attempts and no score penalties (unlike other professors that I have known). ", "12/14/2018 10:30:12")
-# review_3 = Review(8, "Hill is hard. Try to rack up points on the quizzes and just practice alot for the midterms and final. ", "12/19/2017 11:41:30")
-# review_4 = Review(9, "Avoid Bartels at all costs. This man singlehandedly made my quarter hell. Midterms are all free response sketches and you only get 30 minutes to finish. The grading system was a mess. COVID really inspired this professor to make everything even more confusing than it should be. He basically hid our grades and made them impossible to calculate until after the P/NP deadline. He also made the final exam an oral interview. If you take this class with Bartels, also expect to go beyond Chem1A, because he starts teaching OChem stuff like Mass Spectrometry. He also gives out weekly discussion homework and homework every day (including holidays and weekends). Lecture clickers are graded on accuracy. Super difficult class for intro chem.", "12/11/2020 14:08:58")
-# review_list = [review_1, review_2, review_3, review_4]
-# course_1 = Course("CHEM001A", 5.66, review_list)
-# print("avg_difficulty:", course_1.avg_difficulty)
-# print("avg_sentiment", course_1.avg_sentiment)
-# print("comment summary:", course_1.comment_summary)
-# print("course_id:", course_1.course_id)
-# print("department:", course_1.department)
--- data/tests.py
+++ data/tests.py
@@ -12,6 +12,8 @@ if __name__ == "__main__":
     print("review_1: text test passed")
     assert review_1.date_posted == "3/28/2017 15:43:18", "Incorrect date posted"
     print("review_1: date test passed")
+    assert review_1.sentiment_score <= 1 and review_1.sentiment_score >= -1, "Sentiment score is not in the correct range"
+    print("review_1: sentiment_score in the appropriate range")
     print("review_1 sentiment_score:", review_1.sentiment_score)
 
     review_2 = Review(1, "No tests except Final is writing a story", "3/28/2017 22:41:05")
@@ -21,6 +23,8 @@ if __name__ == "__main__":
     assert review_2.text == "No tests except Final is writing a story", "Incorrect text"
     assert review_2.date_posted == "3/28/2017 22:41:05", "Incorrect date posted"
     print("review_2: date test passed")
+    assert review_2.sentiment_score <= 1 and review_2.sentiment_score >= -1, "Sentiment score is not in the correct range"
+    print("review_2: sentiment_score in the appropriate range")
     print("review_2 sentiment_score:", review_2.sentiment_score)
 
     review_3 = Review(6, "Brandon Williams, what a guy. Lecture is 3 hours w sign in sheet. " \
@@ -50,5 +54,20 @@ if __name__ == "__main__":
     print("review_3: text test passed")
     assert review_3.date_posted == "12/17/2019 10:58:00", "Incorrect date posted"
     print("review_3: date test passed")
+    assert review_3.sentiment_score <= 1 and review_3.sentiment_score >= -1, "Sentiment score is not in the correct range"
+    print("review_3: sentiment_score in the appropriate range")
     print("review_3 sentiment_score:", review_3.sentiment_score)
-    pass
+
+    # test Course class
+    # review_1 = Review(4, "Very general chemistry, just know your stuff", "3/29/2017 14:10:31")
+    # review_2 = Review(4, "If you passed the AP Chemistry Test in high school, this class will be a good review for you since it will cover topics from the different models of atoms, types of bonding, intermolecular forces, and stoichiometry. (You should still cover the material so you have a good foundation and an easy time in the class though) If possible, I would recommend taking it with Fokwa. Has a thick accent but sets up the class in a way that makes it super difficult to fail (you seriously need to not do anything to not get points). He has a redemption system so you can get points back if your final exam grade is much higher than your midterm exam scores. All of his exams have extra credit problems that he will give you to do before the exam day (yay!), but he has no curve. So scores on the exams depend entirely up to you. Homework is an easy 100% as long as you do it before the deadline and gives you unlimited attempts and no score penalties (unlike other professors that I have known). ", "12/14/2018 10:30:12")
+    # review_3 = Review(8, "Hill is hard. Try to rack up points on the quizzes and just practice alot for the midterms and final. ", "12/19/2017 11:41:30")
+    # review_4 = Review(9, "Avoid Bartels at all costs. This man singlehandedly made my quarter hell. Midterms are all free response sketches and you only get 30 minutes to finish. The grading system was a mess. COVID really inspired this professor to make everything even more confusing than it should be. He basically hid our grades and made them impossible to calculate until after the P/NP deadline. He also made the final exam an oral interview. If you take this class with Bartels, also expect to go beyond Chem1A, because he starts teaching OChem stuff like Mass Spectrometry. He also gives out weekly discussion homework and homework every day (including holidays and weekends). Lecture clickers are graded on accuracy. Super difficult class for intro chem.", "12/11/2020 14:08:58")
+    # review_list = [review_1, review_2, review_3, review_4]
+    # course_1 = Course("CHEM001A", 5.66, review_list)
+    # print("avg_difficulty:", course_1.avg_difficulty)
+    # print("avg_sentiment", course_1.avg_sentiment)
+    # print("comment summary:", course_1.comment_summary)
+    # print("course_id:", course_1.course_id)
+    # print("department:", course_1.department)
+    print("All tests passed")
