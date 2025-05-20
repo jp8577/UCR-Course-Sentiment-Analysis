341070b27ed305e61259314707b4587039955327
Riceko <nathanbchenwang@gmail.com>
2025-05-20 12:21:41 -0700

    navbar completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { ReactQueryClientProvider } from "@/utils/react-query";
-
+import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -17,6 +17,7 @@ export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar />
         <ReactQueryClientProvider>{children}</ReactQueryClientProvider>
       </body>
     </html>
