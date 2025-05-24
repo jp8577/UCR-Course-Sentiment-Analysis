c7d9719ce6b093793af4bf99d84ee700ec0c6f2a
cmakkkk <146979512+cmakkkk@users.noreply.github.com>
2025-05-24 00:27:58 -0700

    Saving so I don't lose progress

--- data/Course.py
+++ data/Course.py
@@ -6,6 +6,8 @@ from data.Review import Review
 # pip install torch
 
 import transformers
+import re
+import torch
 from transformers import pipeline
 # summarizer = pipeline('summarization')
 
@@ -18,6 +20,8 @@ class Course:
         self.department = self.__calculate_department(self.course_id)
         self.avg_sentiment = self.__calculate_avg_sentiment(self.review_list)
         self.comment_summary = self.__summarize_comments(self.review_list)
+        self.__reviews_summarized = len(self.review_list)
+        self.__unsummarized_words = 0
 
     def __calculate_department(self, course_id):
         letters = ""
@@ -34,31 +38,77 @@ class Course:
         sum = 0
         counter = 0
         for review in reviews:
-            sum += review.rating
+            sum += review.sentiment_score
             counter += 1
         return (sum / counter)
 
 
     def __summarize_comments(self, review_list):
+        '''
+        The pipeline we're using cannot handle more than 1024 tokens at once which is about 700-800 words.
+        Because of this, we'll have to summarize new reviews with the current summary so that we're never
+        summarizing more than 700 words at once. Instead of summarizing all the reviews at once, we must
+        summarize a few reviews at a time with the the current running summary.
+
+        This will not work if a Course object is initialized with a comments worth more than 
+        1024 tokens. However, our current implementation adds reviews one by one so this shouldn't
+        be a problem.
+        '''
         if (len(review_list) == 0): return ""
         combined_comments = ""
-        for review in review_list:
+        for review in review_list[self.__reviews_summarized:]:
             combined_comments = combined_comments + " " + review.text
 
-        summarizer = pipeline('summarization')
-        summary = summarizer(combined_comments, max_length=130, min_length=25, do_sample=False)
+        device = 0 if torch.cuda.is_available() else -1
+        summarizer = pipeline('summarization', device=device)
+        words = re.findall(r'\b\w+\b', combined_comments)
+        summary = summarizer(combined_comments, max_length=len(words), min_length=0, do_sample=False)
         return summary[0]['summary_text']
       
     # Methods that needed to be implemented for Database class
     def add_review(self, review):
+        '''
+        When we add a review, add the review to the list of reviews, review_list. We'll also need to 
+        recalculate the average sentiment for the course.
+
+        Summary recalculation is tricky. The pipeline we're using cannot process more than 1024 tokens
+        at once. 1024 tokens is approximately 700-800 words. When adding a review, we won't itegrate it
+        into the comment until we're about to go over the 700 word limit or we need an up to date comment
+        summary (see the comment in get_comment_summary).
+        '''
+
+        # if the review puts us over the 700 word limit, summarize the current summary with the unsummarized reviews
+        # NEVER SUMMARIZE MORE THAN 700 WORDS AT ONCE
+        num_words = len(re.findall(r'\b\w+\b', review.text))
+        if (self.__unsummarized_words + num_words >= 700):
+            self.comment_summary = self.__summarize_comments(self.review_list)
+            self.__unsummarized_words = 0
+
+        # add the review to review_list
         self.review_list.append(review)
-        #self.update_avg_difficulty()
+        
+        # increment the word counter
+        self.__unsummarized_words = num_words + len(re.findall(r'\b\w+\b', self.comment_summary))
+        
+        # update the other stuff: avg_sentiment and avg_difficulty
+        self.avg_sentiment = self.__calculate_avg_sentiment(self.review_list)
+        self.update_avg_difficulty()
         
 
     def get_avg_sentiment(self):
         return self.avg_sentiment
     
     def get_comment_summary(self):
+        '''
+        If the current summary summarizes all of the reviews in review list, when we just return the summary.
+        When adding a review, we don't summarize immediately in order to save time and information.
+        If get_comment_summary is called, we want an up to date summary no matter what. The if condition
+        checks if we're up to date. If the summary is up to date, we return the comment and if it's not up
+        to date, we make it up to date.
+        '''
+        if (self.__reviews_summarized != len(self.review_list)):
+            self.comment_summary = self.__summarize_comments(self.review_list)
+            self.__reviews_summarized = len(self.review_list)
         return self.comment_summary
     
     def update_avg_difficulty(self):
